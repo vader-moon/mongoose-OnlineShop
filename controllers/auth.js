d8184d99a2639e19917e07456ff3c6ff43c6f267
Andrew Essex <aessex@tmcdesign.com>
2019-01-28 15:41:34 -0700

    add session cookie to login

--- controllers/auth.js
+++ controllers/auth.js
@@ -1,19 +1,13 @@
 exports.getLogin = (req, res, next) => {
-  const isLoggedIn = req.
-  get('Cookie')
-  .split(';')[1]
-  .trim()
-  .split('=')[1];
-  console.log(isLoggedIn);
-
+console.log(req.session.isLoggedIn);
   res.render('auth/login', {
       docTitle: 'Login', 
       path: '/login',
-      isAuthenticated: isLoggedIn
+      isAuthenticated: false
   }); // Allows to send a response and attach a body of type any.
 };
 
 exports.postLogin = (req, res, next) => {
-  res.setHeader('Set-Cookie', 'isLoggedIn=true');
+  req.session.isLoggedIn = true;
   res.redirect('/');
 };
